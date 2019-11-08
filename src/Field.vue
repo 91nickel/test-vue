b39572bbc92fec32422fa49674ea5daaf1a60caf
91nickel <43931371+91nickel@users.noreply.github.com>
2019-11-08 18:15:58 +0300

    Initial commit

---
+++ src/Field.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <h3>{{msg}}</h3>
+    <input type="text" :value="msg" v-on:input="changeMess">
+    <p>{{ mess }}</p>
+  </div>
+</template>
+
+<script>
+    export default {
+        props: ['msg'],
+        data() {
+            return {
+                mess: ''
+            };
+        },
+        methods: {
+            changeMess(event){
+                this.mess = event.target.value;
+                this.$emit('messChange', this.mess);
+            }
+        }
+    }
+</script>
